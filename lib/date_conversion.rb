b7237045ebc1a70f6cb2e822e7389a6a23c4c973
Chris Caragianis <chriscaragianis@gmail.com>
2016-04-22 15:35:48 -0400

    placeholder background

---
+++ lib/date_conversion.rb
@@ -0,0 +1,10 @@
+require 'date'
+
+date = Date.today
+puts date.to_time.to_i * 1000
+date += 1
+puts date.to_time.to_i * 1000
+date += 1
+puts date.to_time.to_i * 1000
+date += 1
+puts date.to_time.to_i * 1000
